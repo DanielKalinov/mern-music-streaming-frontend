3b6236d28870b87ac762eddd98651d2813a0ad31
daniel <danielkalinov@protonmail.com>
2022-11-03 15:10:15 +0200

    added progress handle

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import axios from 'axios';
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import Slider from 'rc-slider';
 import 'rc-slider/assets/index.css';
 
 const App = () => {
 	const [url, setUrl] = useState();
+	const [percent, setPercent] = useState(0);
+	const audioRef = useRef();
 
 	useEffect(() => {
 		axios.get('http://localhost:5000/test_audio').then((res) => {
@@ -12,23 +14,41 @@ const App = () => {
 		});
 	}, []);
 
+	const handleProgress = () => {
+		const percent =
+			(audioRef.current.currentTime / audioRef.current.duration) * 100;
+
+		setPercent(percent);
+	};
+
 	return (
 		<>
-			<audio src={url} controls />
-			<Slider
-				min={1}
-				max={80}
-				railStyle={{ cursor: 'pointer' }}
-				trackStyle={[{ backgroundColor: 'red', cursor: 'pointer' }]}
-				handleStyle={[
-					{
-						backgroundColor: 'red',
-						borderColor: 'red',
-						opacity: 1,
-						cursor: 'pointer',
-					},
-				]}
+			<audio
+				ref={audioRef}
+				src={url}
+				controls
+				onTimeUpdate={handleProgress}
+				muted
 			/>
+			<div style={{ width: '300px' }}>
+				<Slider
+					value={percent}
+					railStyle={{ cursor: 'pointer' }}
+					trackStyle={[{ backgroundColor: 'red', cursor: 'pointer' }]}
+					handleStyle={[
+						{
+							backgroundColor: 'red',
+							borderColor: 'red',
+							cursor: 'pointer',
+						},
+					]}
+					onChange={(value) => {
+						setPercent(value);
+						audioRef.current.currentTime =
+							(value / 100) * audioRef.current.duration;
+					}}
+				/>
+			</div>
 		</>
 	);
 };
