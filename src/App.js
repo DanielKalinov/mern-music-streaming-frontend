da3fcb5fcfc3150e2a183e9c0985a60a692c5470
daniel <danielkalinov@protonmail.com>
2022-11-06 11:57:35 +0200

    moved Slider to AudioControlsPanel

--- src/App.js
+++ src/App.js
@@ -20,25 +20,8 @@ const App = () => {
 				onClick={() => (audio.paused ? audio.play() : audio.pause())}>
 				Play
 			</div>
-			<div style={{ width: '300px' }}>
-				<Slider
-					railStyle={{ backgroundColor: '#71717a', cursor: 'pointer' }}
-					trackStyle={[{ backgroundColor: '#db2777', cursor: 'pointer' }]}
-					handleStyle={[
-						{
-							backgroundColor: '#db2777',
-							borderColor: '#db2777',
-							cursor: 'pointer',
-							boxShadow: 'none',
-							opacity: 1,
-						},
-					]}
-					onAfterChange={(value) => {
-						audio.currentTime = (value / 100) * audio.duration;
-					}}
-				/>
-			</div>
-			<AudioControlsPanel />
+
+			<AudioControlsPanel audio={audio} />
 		</>
 	);
 };
--- src/components/AudioControlsPanel/AudioControlsPanel.jsx
+++ src/components/AudioControlsPanel/AudioControlsPanel.jsx
@@ -1,7 +1,27 @@
+import Slider from 'rc-slider';
 import React from 'react';
 
-const AudioControlsPanel = () => {
-	return <div>AudioControlsPanel</div>;
+const AudioControlsPanel = ({ audio }) => {
+	return (
+		<div style={{ width: '300px' }}>
+			<Slider
+				railStyle={{ backgroundColor: '#71717a', cursor: 'pointer' }}
+				trackStyle={[{ backgroundColor: '#db2777', cursor: 'pointer' }]}
+				handleStyle={[
+					{
+						backgroundColor: '#db2777',
+						borderColor: '#db2777',
+						cursor: 'pointer',
+						boxShadow: 'none',
+						opacity: 1,
+					},
+				]}
+				onAfterChange={(value) => {
+					audio.currentTime = (value / 100) * audio.duration;
+				}}
+			/>
+		</div>
+	);
 };
 
 export default AudioControlsPanel;
