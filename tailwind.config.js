f9281ed6dd6d1e701ba2551a00845ff6a96e9b7f
Daniel Kalinov <danielkalinov@protonmail.com>
2023-12-06 14:32:59 +0200

    added hover to card class

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,7 @@ module.exports = {
 			colors: {
 				'background-dark': colors.slate[900],
 				primary: colors.slate[800],
-				secondary: colors.slate[600],
+				secondary: colors.slate[700],
 				accent: colors.yellow[300],
 				inactive: colors.slate[400],
 				disabled: colors.slate[500],
