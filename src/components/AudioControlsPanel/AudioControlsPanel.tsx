c3f68fde762a6ca78826424c92b7e58730c82f20
Daniel Kalinov <danielkalinov@protonmail.com>
2023-12-06 18:17:21 +0200

    added max-width to audioControlsPanel

--- src/components/AudioControlsPanel/AudioControlsPanel.tsx
+++ src/components/AudioControlsPanel/AudioControlsPanel.tsx
@@ -56,7 +56,7 @@ const AudioControlsPanel = ({
 						? 'translate-y-0'
 						: 'translate-y-full'
 				} fixed bottom-0 left-0 px-2 pb-2 w-full transition-all duration-300 ease-in-out z-20`}>
-				<div className='overflow-hidden flex flex-col items-center card'>
+				<div className='flex flex-col max-w-xl m-auto items-center card overflow-hidden'>
 					<div
 						className='relative flex items-center justify-between w-full'
 						onClick={() => setShowTrackInfo(true)}>
