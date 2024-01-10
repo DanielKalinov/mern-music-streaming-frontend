288c7b2e1465899f38c3ee7e995ab4470696d9f7
Daniel Kalinov <danielkalinov@protonmail.com>
2024-01-10 20:47:08 +0200

    removed toUpperCase from artist names

--- src/utils/artistName.ts
+++ src/utils/artistName.ts
@@ -1,11 +1,8 @@
 function artistNames(artist: { name: string }[]) {
 	if (artist) {
 		return artist.length > 1
-			? artist
-					.map((item) => item.name)
-					.join(', ')
-					.toUpperCase()
-			: artist[0].name.toUpperCase();
+			? artist.map((item) => item.name).join(', ')
+			: artist[0].name;
 	}
 
 	return '';
