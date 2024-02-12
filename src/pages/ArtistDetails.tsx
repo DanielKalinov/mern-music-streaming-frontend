4a2d668972acd15b26cc987cdfd6c11d61175077
Daniel Kalinov <danielkalinov@protonmail.com>
2024-02-12 16:00:11 +0200

    Added DiscographySection component

--- src/pages/ArtistDetails.tsx
+++ src/pages/ArtistDetails.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useRef, useState } from 'react';
 import axios from 'axios';
-import { Link, useParams } from 'react-router-dom';
+import { useParams } from 'react-router-dom';
 import PageTransition from '../components/PageTransition';
 import NavigationBar from '../components/NavigationBar';
 import Image from '../components/Image';
@@ -10,6 +10,7 @@ import PlaylistControls from '../components/PlaylistControls';
 import { useSelector } from 'react-redux';
 import AudioPlayerState from '../types/AudioPlayerState';
 import BioSection from '../components/BioSection';
+import DiscographySection from '../components/DiscographySection';
 
 const ArtistDetails = () => {
 	const [artistDetails, setArtistDetails] = useState<Artist>();
@@ -90,56 +91,10 @@ const ArtistDetails = () => {
 							</div>
 						</div>
 					</div>
-					<div className='mt-8'>
-						<h2 className='mb-4'>Discography</h2>
-						<ul className='grid grid-cols-1 gap-3 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4'>
-							{artistDetails?.albums
-								?.slice()
-								.sort((a, b) => (a.year > b.year ? -1 : 1))
-								.map(({ _id, name, albumImageUrl, tracks, year }) => (
-									<li
-										key={_id}
-										className='relative rounded-lg shadow-card overflow-hidden group'>
-										<div className='absolute top-0 left-0 h-full w-full backdrop-blur-0'>
-											<div className='absolute top-0 left-0 h-full w-full bg-gradient-to-r from-primary/70 to-primary backdrop-blur-3xl backdrop-saturate-200 z-0' />
-											<div
-												className='h-full w-full'
-												style={{
-													backgroundImage: `url('${albumImageUrl}')`,
-													backgroundPosition: 'center',
-												}}
-											/>
-										</div>
-										<Link
-											to={`/albums/${_id}`}
-											className='relative flex items-center p-2 transition-colors duration-150 group group-hover:bg-secondary'>
-											<Image
-												src={albumImageUrl}
-												classes='shrink-0 h-[70px] w-[70px] rounded-tl-lg rounded-lg shadow-img lg:h-[100px] lg:w-[100px]'
-											/>
-											<div className='ml-4 overflow-hidden'>
-												<span
-													className={`truncate block text-sm font-semibold ${
-														currentPlaylistInfo.name == name && 'text-accent'
-													}`}>
-													{name}
-												</span>
-												<span className='block text-sm text-inactive'>
-													{year} • {tracks.length} tracks
-												</span>
-											</div>
-										</Link>
-										<div
-											className={`opacity-0 translate-y-2 absolute bottom-2 left-2 duration-300 group-hover:opacity-100 group-hover:translate-y-0`}>
-											<PlaylistControls
-												playlist={tracks}
-												playlistInfo={{ type: 'album', name }}
-											/>
-										</div>
-									</li>
-								))}
-						</ul>
-					</div>
+					<DiscographySection
+						artistDetails={artistDetails ?? null}
+						currentPlaylistInfo={currentPlaylistInfo}
+					/>
 					<div className='mt-8 md:hidden'>
 						<BioSection
 							artistDetails={{
