d71577682bb119e805425f2b7359b0cee8f1c287
Daniel Kalinov <danielkalinov@protonmail.com>
2024-02-17 18:14:29 +0200

    changed artist page style again

--- src/pages/Artists.tsx
+++ src/pages/Artists.tsx
@@ -6,6 +6,7 @@ import Image from '../components/Image';
 import Artist from '../types/Artist';
 import { useSelector } from 'react-redux';
 import AudioPlayerState from '../types/AudioPlayerState';
+import PlaylistControls from '../components/PlaylistControls';
 
 const Artists = () => {
 	const [artists, setArtists] = useState<Artist[]>([]);
@@ -22,25 +23,38 @@ const Artists = () => {
 		<PageTransition duration={1}>
 			<div className='pageContainer'>
 				<h1 className='w-full text-center mt-4 mb-8'>Pick an artist</h1>
-				<div className='grid grid-cols-3 gap-4 sm:grid-cols-4 md:grid-cols-5 lg:grid-cols-6'>
-					{artists.map((item: Artist) => (
-						<Link key={item._id} to={`/artist/${item._id}`}>
-							<div className='rounded-lg transition-all duration-300 ease-in-out group hover:bg-primary hover:shadow-card hover:p-2'>
-								<div className='transition-all duration-300 ease-in-out group-hover:p-2'>
-									<Image
-										src={item.artistImageUrl}
-										fullSize={true}
-										classes='aspect-square rounded-full shadow-card'
-									/>
+				<div className='grid grid-cols-3 sm:grid-cols-4 md:grid-cols-5 lg:grid-cols-6'>
+					{artists.map(({ _id, name, artistImageUrl, tracks }: Artist) => (
+						<div key={_id} className='relative group'>
+							<Link to={`/artist/${_id}`}>
+								<div className='p-4 rounded-lg transition-all duration-300 ease-in-out group disablemobilehover:group-hover:bg-primary disablemobilehover:group-hover:shadow-card'>
+									<div className='transition-all duration-300 ease-in-out disablemobilehover:group-hover:p-2'>
+										<Image
+											src={artistImageUrl}
+											fullSize={true}
+											classes='aspect-square rounded-full shadow-card'
+										/>
+									</div>
+									<span
+										className={`mt-4 block text-center text-sm font-semibold truncate sm:text-base md:text-lg ${
+											currentPlaylistInfo.name == name && 'text-accent'
+										}`}>
+										{name}
+									</span>
 								</div>
-								<span
-									className={`mt-2 block text-center text-sm font-semibold truncate ${
-										currentPlaylistInfo.name == item.name && 'text-accent'
-									}`}>
-									{item.name}
-								</span>
+							</Link>
+							<div
+								className={`absolute bottom-16 right-8 transition-all duration-300 disablemobilehover:group-hover:visible disablemobilehover:group-hover:opacity-100 ${
+									currentPlaylistInfo.name == name
+										? 'visible opacity-100 translate-y-0'
+										: 'invisible opacity-0 translate-y-2'
+								}`}>
+								<PlaylistControls
+									playlist={tracks}
+									playlistInfo={{ type: 'artist', name: name }}
+								/>
 							</div>
-						</Link>
+						</div>
 					))}
 				</div>
 			</div>
