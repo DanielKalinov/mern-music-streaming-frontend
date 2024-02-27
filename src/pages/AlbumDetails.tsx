021afb3eb0025add1dceb39216d846770f9e9eff
Daniel Kalinov <danielkalinov@protonmail.com>
2024-02-27 19:44:13 +0200

    changed album page structure

--- src/pages/AlbumDetails.tsx
+++ src/pages/AlbumDetails.tsx
@@ -26,49 +26,50 @@ const AlbumDetails = () => {
 
 	return albumDetails ? (
 		<PageTransition duration={0.2}>
-			<div className='max-w-2xl m-auto'>
-				<NavigationBar
-					text={albumDetails.name}
-					targetRef={targetRef}
-					threshold={20}
-				/>
-				<div className='mt-20 mb-8 sm:flex sm:items-center sm:gap-6'>
-					<div className='absolute top-0 left-0 w-full'>
+			<NavigationBar
+				text={albumDetails.name}
+				targetRef={targetRef}
+				threshold={20}
+			/>
+			<div className='-mx-4'>
+				<div className='relative w-full pt-20 pb-8'>
+					<div className='absolute top-0 left-0 w-full h-full bg-gradient-to-b from-transparent to-background-dark backdrop-blur-3xl z-10' />
+					<div
+						className='absolute top-0 left-0 h-full w-full'
+						style={{
+							backgroundImage: `url(${albumDetails.albumImageUrl})`,
+							backgroundSize: 'cover',
+						}}
+					/>
+					<div className='relative max-w-2xl m-auto px-4 z-20 sm:flex sm:items-center sm:gap-6'>
 						<Image
 							src={albumDetails.albumImageUrl}
-							noPlaceholder
-							fullSize={true}
-							classes='max-h-[400px]'
+							height={250}
+							width={250}
+							classes='shrink-0 m-auto w-fit mb-6 shadow-card rounded-lg sm:m-0'
 						/>
-						<div className='absolute top-0 left-0 h-full w-full bg-black/30 backdrop-blur-[128px]' />
-						<div className='absolute top-0 left-0 h-full w-full bg-gradient-to-b from-transparent to-background-dark' />
-					</div>
-					<Image
-						src={albumDetails.albumImageUrl}
-						height={250}
-						width={250}
-						classes='shrink-0 m-auto w-fit mb-6 shadow-card rounded-lg sm:m-0'
-					/>
-					<div
-						ref={targetRef}
-						className='relative flex items-center justify-between w-full mb-8 transition-opacity duration-200 h-fit sm:mb-0'>
-						<div>
-							<h2 className='mb-1'>{albumDetails.name}</h2>
-							<span className='text-inactive'>
-								{artistNames(albumDetails.artist)}
-							</span>
-							<span className='block text-inactive'>
-								{albumDetails.year} • {albumDetails.tracks.length} tracks,{' '}
-								{albumDetails.duration}
-							</span>
+						<div
+							ref={targetRef}
+							className='relative flex items-center justify-between w-full transition-opacity duration-200'>
+							<div>
+								<h2 className='mb-1'>{albumDetails.name}</h2>
+								<span className='text-inactive'>
+									{artistNames(albumDetails.artist)}
+								</span>
+								<span className='block text-inactive'>
+									{albumDetails.year} • {albumDetails.tracks.length} tracks,{' '}
+									{albumDetails.duration}
+								</span>
+							</div>
+							<PlaylistControls
+								playlist={albumDetails.tracks}
+								playlistInfo={{ type: 'album', name: albumDetails.name }}
+							/>
 						</div>
-						<PlaylistControls
-							playlist={albumDetails.tracks}
-							playlistInfo={{ type: 'album', name: albumDetails.name }}
-						/>
 					</div>
 				</div>
-
+			</div>
+			<div className='max-w-2xl m-auto'>
 				<TrackList
 					tracks={albumDetails.tracks}
 					playlistInfo={{ type: 'album', name: albumDetails.name }}
